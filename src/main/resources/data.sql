85028f6d9617c3a6b470f512d5e31c58ab5cc75e
Maksim Torzhkov <maxtorj@yandex.ru>
2025-07-27 19:00:12 +0300

    feat: update schema and data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,5 @@
--- Фильмы
-INSERT INTO films (film_id, name, description, release_date, duration, mpa_id)
-VALUES
-    (1, 'Film 1', 'Description 1', '2000-01-01', 120, 1),
-    (2, 'Film 2', 'Description 2', '2010-05-15', 90, 2);
-
--- MPA рейтинги
+-- 1. Сначала заполняем таблицы без внешних ключей
+-- Рейтинги MPA
 INSERT INTO mpa_ratings (mpa_id, name, description)
 VALUES
     (1, 'G', 'Без возрастных ограничений'),
@@ -16,13 +11,6 @@ VALUES
     (1, 'Комедия'),
     (2, 'Драма');
 
--- Связь фильмов и жанров
-INSERT INTO film_genre (film_id, genre_id)
-VALUES
-    (1, 1),  -- Film 1: Комедия
-    (1, 2),  -- Film 1: Драма (если у фильма несколько жанров)
-    (2, 2);  -- Film 2: Драма
-
 -- Пользователи
 INSERT INTO users (user_id, email, login, name, birthday)
 VALUES
@@ -30,13 +18,28 @@ VALUES
     (2, 'user2@mail.ru', 'user2', 'User Two', '1995-05-15'),
     (3, 'user3@mail.ru', 'user3', 'User Three', '2000-10-20');
 
--- Друзья (односторонняя связь)
+-- 2. Затем заполняем таблицы с внешними ключами
+-- Фильмы
+INSERT INTO films (film_id, name, description, release_date, duration, mpa_id)
+VALUES
+    (1, 'Film 1', 'Description 1', '2000-01-01', 120, 1),
+    (2, 'Film 2', 'Description 2', '2010-05-15', 90, 2);
+
+-- 3. В конце - таблицы связей
+-- Жанры фильмов
+INSERT INTO film_genre (film_id, genre_id)
+VALUES
+    (1, 1),  -- Film 1: Комедия
+    (1, 2),  -- Film 1: Драма
+    (2, 2);  -- Film 2: Драма
+
+-- Друзья
 INSERT INTO friends (user_id, friend_id)
 VALUES
     (1, 2),  -- user1 добавил user2
     (1, 3);  -- user1 добавил user3
 
--- Лайки фильмов
+-- Лайки
 INSERT INTO likes (film_id, user_id)
 VALUES
     (1, 1), (1, 2),
